9f848ea66bf2939cab1bd8246c61eea4fa70997a
shauryaagarwal2007 <shaurya.ghy@gmail.com>
2021-01-06 15:40:45 +0530

    platform

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
+    box01 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
@@ -44,7 +44,7 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
-    box1.display();
+    box01.display();
     box2.display();
     ground.display();
     pig1.display();
